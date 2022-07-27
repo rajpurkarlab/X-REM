e1b51cca19d78dff0528db4e535700d658b2e563
Vignav Ramesh <rvignav@gmail.com>
2022-07-27 00:19:38 -0700

    Add option not to use ve

--- ALBEF/CXR_ReFusE_pipeline.py
+++ ALBEF/CXR_ReFusE_pipeline.py
@@ -1,7 +1,5 @@
 import pandas as pd
 from CXR_ReFusE_module import RETRIEVAL_MODULE
-import os
-from tqdm import tqdm
 import argparse
 
 def main(args):
@@ -18,25 +16,29 @@ def main(args):
                                          max_token_len = 25)
     cosine_sim_output = cosine_sim_module.predict()
 
-    new_impressions = [el.split('|') for el in cosine_sim_output['Report Impression']]
-   
-    ve_module = RETRIEVAL_MODULE(impressions=new_impressions, 
-                                            mode='visual-entailment', 
-                                            config=args.albef_ve_config, 
-                                            checkpoint=args.albef_ve_ckpt,
-                                            topk=args.albef_ve_top_k,
-                                            input_resolution=384, 
-                                            img_path=args.img_path, 
-                                            delimiter='[SEP]', 
-                                            max_token_len=30)
-    ve_output = ve_module.predict()
-    ve_output.to_csv(args.save_path, index = False)
+    if args.use_ve:
+        new_impressions = [el.split('|') for el in cosine_sim_output['Report Impression']]
+    
+        ve_module = RETRIEVAL_MODULE(impressions=new_impressions, 
+                                                mode='visual-entailment', 
+                                                config=args.albef_ve_config, 
+                                                checkpoint=args.albef_ve_ckpt,
+                                                topk=args.albef_ve_top_k,
+                                                input_resolution=384, 
+                                                img_path=args.img_path, 
+                                                delimiter='[SEP]', 
+                                                max_token_len=30)
+        ve_output = ve_module.predict()
+        ve_output.to_csv(args.save_path, index = False)
+    else:
+        cosine_sim_output.to_csv(args.save_path, index = False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--impressions_path', default='../data/mimic_train_impressions.csv', help= 'path to the mimic train corpus')
     parser.add_argument('--img_path', default='../data/cxr.h5', help = 'path to the test file') 
     parser.add_argument('--save_path', default='example.csv')
+    parser.add_argument('--use_ve', default=False)
     parser.add_argument('--albef_retrieval_config', default='configs/Retrieval_flickr.yaml')
     parser.add_argument('--albef_retrieval_ckpt', default='output/sample/pretrain/checkpoint_59.pth')
     parser.add_argument('--albef_retrieval_top_k', type = int, default=50)
@@ -44,4 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('--albef_ve_ckpt', default='output/sample/ve/checkpoint_7.pth')    
     parser.add_argument('--albef_ve_top_k', default=10, type = int, help='url used to set up distributed training')
     args = parser.parse_args()
+
+    if not args.use_ve:
+        args.albef_retrieval_top_k = 1
+    
     main(args)
