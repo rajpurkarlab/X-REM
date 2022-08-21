296f7335ef9e15ea6cc434edb707011fd5d0337a
Jeong <jj229@login01.o2.rc.hms.harvard.edu>
2022-08-21 17:23:12 -0400

    removed shellscripts

--- inference_no_nli.sh
+++
@@ -1,9 +0,0 @@
-source ~/.bashrc
-
-module load conda2/4.2.13
-module load gcc/6.2.0
-module load cuda/11.2
-
-cd ALBEF
-
-python3 CXR_ReFusE_pipeline.py --albef_ve_delimiter ' ' --save_path no_nli.csv --albef_ve_top_k 1
