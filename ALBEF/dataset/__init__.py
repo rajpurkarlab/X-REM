da5f325c9b29928d88a28f5f3f7ff49376352adc
Jeong <jj229@compute-g-17-145.o2.rc.hms.harvard.edu>
2022-11-07 00:02:48 -0500

    deleted irrelevant ALBEF files

--- ALBEF/dataset/__init__.py
+++ ALBEF/dataset/__init__.py
@@ -4,10 +4,7 @@ from torchvision import transforms
 from PIL import Image
 
 from dataset.caption_dataset import re_train_dataset, re_eval_dataset, pretrain_dataset
-from dataset.nlvr_dataset import nlvr_dataset
 from dataset.ve_dataset import ve_dataset
-from dataset.vqa_dataset import vqa_dataset
-from dataset.grounding_dataset import grounding_dataset
 
 from dataset.randaugment import RandomAugment
 
@@ -16,24 +13,14 @@ def create_dataset(dataset, config):
     normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
     
     pretrain_transform = transforms.Compose([                        
-#             transforms.RandomResizedCrop(config['image_res'],scale=(0.2, 1.0), interpolation=Image.BICUBIC),
-#             transforms.RandomHorizontalFlip(),
-#             RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
-#                                               'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
             transforms.ToTensor(),
             normalize,
         ])    
     train_transform = transforms.Compose([                        
-#             transforms.RandomResizedCrop(config['image_res'],scale=(0.5, 1.0), interpolation=Image.BICUBIC),
-#             transforms.RandomHorizontalFlip(),
-#             RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
-#                                               'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
             transforms.ToTensor(),
             normalize,
         ])  
     test_transform = transforms.Compose([
-#         transforms.Resize((config['image_res'],config['image_res']),interpolation=Image.BICUBIC),
-#         transforms.ToTensor(),
         normalize,
         ])   
     
@@ -47,16 +34,6 @@ def create_dataset(dataset, config):
         test_dataset = re_eval_dataset(config['test_file'], test_transform, config['image_root'])                
         return train_dataset, val_dataset, test_dataset   
 
-    elif dataset=='vqa': 
-        train_dataset = vqa_dataset(config['train_file'], train_transform, config['vqa_root'], config['vg_root'], split='train') 
-        vqa_test_dataset = vqa_dataset(config['test_file'], test_transform, config['vqa_root'], config['vg_root'], split='test', answer_list=config['answer_list'])       
-        return train_dataset, vqa_test_dataset
-
-    elif dataset=='nlvr':   
-        train_dataset = nlvr_dataset(config['train_file'], train_transform, config['image_root'])  
-        val_dataset = nlvr_dataset(config['val_file'], test_transform, config['image_root'])  
-        test_dataset = nlvr_dataset(config['test_file'], test_transform, config['image_root'])                
-        return train_dataset, val_dataset, test_dataset        
                
     elif dataset=='ve':   
         train_dataset = ve_dataset(config['train_file'], train_transform)  
@@ -64,30 +41,6 @@ def create_dataset(dataset, config):
         test_dataset = ve_dataset(config['test_file'], test_transform)                
         return train_dataset, val_dataset, test_dataset     
     
-    elif dataset=='grounding':
-        train_transform = transforms.Compose([                        
-                transforms.Resize((config['image_res'],config['image_res']),interpolation=Image.BICUBIC),
-                transforms.RandomHorizontalFlip(),
-                RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
-                                                  'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
-                transforms.ToTensor(),
-                normalize,
-            ])         
-        train_dataset = grounding_dataset(config['train_file'], train_transform, config['image_root'], mode='train')       
-        test_dataset = grounding_dataset(config['test_file'], test_transform, config['image_root'], mode='test')             
-        return train_dataset, test_dataset    
-    
-
-def vqa_collate_fn(batch):
-    image_list, question_list, answer_list, weight_list, n = [], [], [], [], []
-    for image, question, answer, weights in batch:
-        image_list.append(image)
-        question_list.append(question)
-        weight_list += weights       
-        answer_list += answer
-        n.append(len(answer))
-    return torch.stack(image_list,dim=0), question_list, answer_list, torch.Tensor(weight_list), n
-
 
 def create_sampler(datasets, shuffles, num_tasks, global_rank):
     samplers = []
